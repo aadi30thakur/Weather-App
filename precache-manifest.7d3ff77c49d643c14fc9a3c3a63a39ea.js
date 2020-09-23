04ff476d12882b22712d59f7fa27ea2694e29660
aadi30thakur <aadi30.thakur@gmail.com>
2020-09-23 16:08:44 +0530

    Updates

---
+++ precache-manifest.7d3ff77c49d643c14fc9a3c3a63a39ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e82c02e38b443f5e31acb8984ce5c0",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "24ecc7143bd5eb57b43c",
+    "url": "/Weather-App/static/css/main.2e18067b.chunk.css"
+  },
+  {
+    "revision": "9f4cc18010cb3c2a0e21",
+    "url": "/Weather-App/static/js/2.cccc4f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.cccc4f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ecc7143bd5eb57b43c",
+    "url": "/Weather-App/static/js/main.3ee3917a.chunk.js"
+  },
+  {
+    "revision": "bd79d7811d6b6bc08c04",
+    "url": "/Weather-App/static/js/runtime-main.2baf0306.js"
+  }
+]);
